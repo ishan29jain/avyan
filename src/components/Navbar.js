15d8b1eb94894d2785a7f53b55666a4635a1d1fd
ishan29jain <jishan29oct@gmail.com>
2024-09-16 11:25:19 +0530

    initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,33 +4,39 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gradient-to-r from-purple-700 to-blue-800 p-5 shadow-lg">
-      <div className="container mx-auto flex justify-between items-center">
-      <a><h1 className="text-3xl font-bold">Avyan</h1></a>  
-        <ul className="flex space-x-6">
-          <li>
-            <a href="#about" className="text-white hover:text-gray-300">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#report-section" className="text-white hover:text-gray-300">
-              Report Incident
-            </a>
-          </li>
-          <li>
-            <a href="#features" className="text-white hover:text-gray-300">
-              Features
-            </a>
-          </li>
-          <li>
-            <a href="#safety-drills" className="text-white hover:text-gray-300">
-              Safety Drills
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+<nav className="bg-gradient-to-r from-purple-700 to-blue-800 p-5 shadow-lg">
+  <div className="container mx-auto flex justify-between items-center">
+    <div className="flex items-center"> {/* Adjusted this container */}
+      <img src="favicon.svg" alt="Logo" className="h-10 w-auto" />
+      <a href="#home" aria-label="Home" className="ml-4">
+        <h1 className="text-3xl font-bold text-white">Avyan</h1>
+      </a>
+    </div>
+    <ul className="flex space-x-6">
+      <li>
+        <a href="#about" className="text-white hover:text-gray-300">
+          About
+        </a>
+      </li>
+      <li>
+        <a href="#report-section" className="text-white hover:text-gray-300">
+          Report Incident
+        </a>
+      </li>
+      <li>
+        <a href="#features" className="text-white hover:text-gray-300">
+          Features
+        </a>
+      </li>
+      <li>
+        <a href="#safety-drills" className="text-white hover:text-gray-300">
+          Safety Drills
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
   );
 };
 
